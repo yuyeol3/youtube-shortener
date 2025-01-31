00fdd190587f90f9a23c3ca54a9632cfdbeda09a
yuyeol3 <zerom50@gmail.com>
2025-01-31 19:01:04 +0900

    heatmap 로직 변경

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const apisRouter = require('./routes/apis');
 const app = express();
 
 app.use(logger(process.env.NODE_ENV));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
--- services/youtubeService.js
+++ services/youtubeService.js
@@ -4,48 +4,24 @@ const { exec } = require("child_process");
 const youtube_url = "https://www.youtube.com/watch?v=";
 const api_url = "https://noembed.com/embed?dataType=json&url=";
 
+const getParseMarkersList = async (vid) => {
+    return new Promise((resolve, reject) => {
+        exec("./fetch " + vid, (error, stdout, stderr)=>{
+            if (error) reject(error);
+            if (stderr) reject(stderr);
+            resolve(stdout);
+        })
+    })
+}
 
-
-const parseMarkersList = async (dom) => {
-    const scriptTags = dom('script').toArray();
-    let markersData = {};
-
-    for (let i = scriptTags.length - 1; i >= 0; i--) {
-        const script = scriptTags[i];
-        const scriptContent = dom(script).html();
-
-        if (scriptContent.includes('"markersList"')) {
-            try {
-                const dataStarts = scriptContent.indexOf('"markersList"');
-                const brackets = {s : 0, m : 0, b : 0};
-                let validContent = false;
-                let result = "";
-                let idx = dataStarts;
-                while ( Object.values(brackets).reduce((p, e)=> p+e) !== 0 ||
-                !validContent ) {
-                    const c = scriptContent.at(idx++);
-                    if (c === "{" || c === "(" || c === "[")
-                        validContent = true;
-
-                    switch (c) {
-                        case '[': brackets.b++; break;
-                        case ']': brackets.b--; break;
-                        case '{': brackets.m++; break;
-                        case '}': brackets.m--; break;
-                        case '(': brackets.s++; break;
-                        case ')': brackets.s--; break;
-                    }
-                    result += c;
-                }
-                markersData = JSON.parse('{' + result + '}');
-                break;
-            } catch (error) {
-                console.error('JSON 파싱 중 오류 발생:', error);
-            }
-        }
+const parseMarkersList = async (vid) => {
+    try {
+        const res = await getParseMarkersList(vid);
+        return JSON.parse('{' + res + '}');
+    } catch (err) {
+        console.error(err);
+        return {};
     }
-
-    return markersData;
 }
 
 const parseTitle = async (vid) => {
@@ -75,18 +51,16 @@ exports.getVidData = async (vid) => {
     }
 
 
-    const res = await fetch(youtube_url + vid,
-        {
-            headers: {"Content-Type": "text/html; charset=UTF-8"}
-        }
-    );
+    // const res = await fetch(youtube_url + vid,
+    //     {
+    //         headers: {"Content-Type": "text/html; charset=UTF-8"}
+    //     }
+    // );
 
-    const html = await res.text();
-    const dom = cheerio.load(html);
+    // const html = await res.text();
+    // const dom = cheerio.load(html);
     return  {
-        markersList : await parseMarkersList(dom),
+        markersList : await parseMarkersList(vid),
         vidTitle : await parseTitle(vid)
     };
-
-
 }
